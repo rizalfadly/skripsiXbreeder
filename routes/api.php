427cc312a6095a5fa0ae606069ee82f8e89e07be
dominic404 <rizalfadli31@gmail.com>
2020-05-27 13:38:02 +0700

    add api untuk vue js

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/person', 'personController@all');
+Route::get('/person/{id}', 'personController@show');
+Route::post('/person', 'personController@store');
+Route::put('/person/{id}', 'personController@update');
+Route::delete('/person/{id}', 'personController@delete');
